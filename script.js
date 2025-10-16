decda297d67bac401f73dbf9e08f9e2989bfc4ca
Fatihmaull <fatihmaulanamail@gmail.com>
2025-10-16 18:35:33 +0800

    add update

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init({
+  duration: 800,
+  once: true,
+});
+
+console.log("💖 Welcome to Hilfa’s Cute Portfolio!");
+
+function toggleCard(card) {
+  card.classList.toggle("active");
+}
+
+function toggleCard(card) {
+  card.classList.toggle("active");
+}
